a04dd5aebe6e4c9ad1cc966b8a9fe7be6c8244cd
RahedulIslam <rahedulislamrh@gmail.com>
2021-01-16 14:41:21 +0600

    This is sample of blog website

---
+++ Project/header.php
@@ -0,0 +1,21 @@
+
+<div class="headeroption">
+   <p style= "color:red ", bgcolor:#f1f1f1> Practice of CRUD</p>
+
+
+</div>
+
+
+<div class="body">
+
+
+
+
+</div>
+
+
+
+<div class='btn btn-info'>
+   <p style=color:#000 , bgcolor:#f1f1f1> Practice of CRUD for www.proyas24.com</p>
+
+</div>
